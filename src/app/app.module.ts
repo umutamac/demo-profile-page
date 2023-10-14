bfa5105b67d768ff46949dd35d2e7df151c75188
UmutMSI <umut@testinvite.com>
2023-10-14 14:56:01 +0300

    tailwind added, sidenav component started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNav } from './components/side-nav/side-nav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNav
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/side-nav/side-nav.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-nav',
+  templateUrl: './side-nav.html',
+  styleUrls: ['./side-nav.css']
+})
+export class SideNav {
+  title = 'ollang';
+}
