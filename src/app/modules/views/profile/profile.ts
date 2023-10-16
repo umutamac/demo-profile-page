19460e57034dffa02bb610686651b08fb96ffe42
umutamac <umut@testinvite.com>
2023-10-16 23:22:24 +0300

    tabs implemented

--- src/app/modules/views/profile/profile.ts
+++ src/app/modules/views/profile/profile.ts
@@ -14,6 +14,8 @@ type User = {
   summary: string
   id: string
 }
+
+type Tab = "experience" | "bio" | "skills" | "portfolio"
 @Component({
   selector: 'profile',
   templateUrl: './profile.html',
@@ -29,4 +31,17 @@ export class Profile implements OnInit {
     console.log(`userId: ${userId}`)
     this.user = userId === null ? undefined : this.list.getUser(userId)
   }
+
+  currentTab: Tab = "experience"
+
+  setTab(tab: Tab) {
+    this.currentTab = tab
+  }
+
+  tabButtons: { text: string, value: Tab }[] = [
+    { text: "Experience", value: "experience" },
+    { text: "Biography", value: "bio" },
+    { text: "Skills", value: "skills" },
+    { text: "Portfolio", value: "portfolio" }
+  ]
 }
